a7d7027ca813576d191911c0ed25bed087b2ee1d
b3n3dict <benedict.xavier.e@gmail.com>
2022-09-16 13:25:08 +0530

    get input values

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
 // Code goes here!
+const form:HTMLElement = document.querySelector('form')!
+const addressInput:HTMLInputElement= document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event:Event){
+  event.preventDefault();
+const enteredAddres = addressInput.value;
+console.log(enteredAddres)
+}
+form?.addEventListener('submit',searchAddressHandler)
